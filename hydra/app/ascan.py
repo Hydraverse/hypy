f0f1d81706945928f49cde0fd0c1c803a9c34635
Halospace Foundation <contact@halospace.org>
2021-12-04 15:58:45 -0800

    ascan: output to and load from progress file

--- hydra/app/ascan.py
+++ hydra/app/ascan.py
@@ -2,6 +2,8 @@
 
 Search transactions for all addresses used as inputs to TXes from this address.
 """
+import os.path
+import sys
 import argparse
 
 from hydra.app.rpc import HydraRPCApp, HydraApp
@@ -16,6 +18,7 @@ from .txvio import TxVIOApp
 @HydraApp.register(name="ascan", desc=__doc__, version="1.01")
 class AScanApp(HydraRPCApp):
     rpc = None
+    out = None
 
     @staticmethod
     def parser(parser: argparse.ArgumentParser):
@@ -23,6 +26,8 @@ class AScanApp(HydraRPCApp):
         parser.add_argument("-c", "--count", type=int, default=10, help="number of recent transactions to load")
         parser.add_argument("-s", "--skip", type=int, default=0, help="number of recent transactions to skip")
         parser.add_argument("-R", "--recursive", action="store_true", help="scan vin addresses recursively")
+        parser.add_argument("-o", "--output",  help="also output to a file (use {} for address in name)")
+        parser.add_argument("-a", "--append", action="store_true", help="load addresses from and append to output file")
         parser.add_argument("address", metavar="ADDR", type=str, help="address to scan")
 
     def run(self):
@@ -30,10 +35,33 @@ class AScanApp(HydraRPCApp):
 
         self.rpc = HydraRPC.__from_parsed__(self.args)
 
+        address = self.args.address
+        addresses = set()
+
+        if self.args.output:
+            filename = self.args.output.replace("{}", address)
+
+            if self.args.append and os.path.isfile(filename):
+
+                with open(filename, "r") as file:
+                    for line in file.readlines():
+                        addr_src, addr_vin = line.strip().split(": ")
+                        addresses.update((addr_vin,))
+                        address = addr_vin
+
+            self.out = open(filename, "a")
+
         try:
-            for (addr_scan, addr_found) in AScanApp.ascan(self.rpc, self.args.address, self.args.count, self.args.skip,
+            for (addr_scan, addr_found) in AScanApp.ascan(self.rpc, address, self.args.count, self.args.skip,
+                                                          addr_found=addresses,
                                                           recursive=self.args.recursive):
-                print(f"{addr_scan}: {addr_found}")
+                line = f"{addr_scan}: {addr_found}\n"
+                print(line, end="")
+                sys.stdout.flush()
+
+                if self.out is not None:
+                    self.out.write(line)
+                    self.out.flush()
 
         except HydraRPC.Error as err:
 
@@ -62,7 +90,7 @@ class AScanApp(HydraRPCApp):
             include_watchonly=True
         )
 
-        logs.info(f"{address}: scanning tx vin addresses...")
+        logs.info(f"{address}: scanning {len(txns)} TXes for vin addresses...")
         for txn in txns:
             logs.debug(f"{address}: txid: {txn.txid}")
             addr_vin, addr_vout = TxVIOApp.get_vinout_addresses(rpc, txn.txid, txn.blockhash)
@@ -80,8 +108,6 @@ class AScanApp(HydraRPCApp):
                     if recursive and addr != address:
                         for (r_addr_scan, r_addr_found) in AScanApp.ascan(rpc, addr, count, skip, addr_found, recursive):
                             yield r_addr_scan, r_addr_found
-
-
 
 
 @Test.register()
