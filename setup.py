308531c6bdb7b6e70a7570bd9f9c93f7bdc63ce0
Halospace Foundation <contact@halospace.org>
2022-01-03 13:11:56 -0800

    [v+] setup.py: properly set up requirements installation

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 from distutils.core import setup
+import pkg_resources
+import pathlib
 import hydra
 import re
 
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
 
 setup(
     name="hypy",
-    version="1.0.7",
+    version="1.1.0",
     description=hydra.__doc__,
     author="Halospace Foundation",
     author_email="contact@halospace.org",
     url="https://github.com/hydraverse/hypy",
-    requires=[re.split("[=<>~]+", line)[0].replace('-', '_') for line in open("requirements.txt").read().splitlines()],
+    install_requires=install_requires,
     packages=["hydra", "hydra.app", "hydra.rpc", "hydra.res", "hydra.test", "hydra.test.app", "hydra.util"],
     package_data={
         "hydra": [
